5eff4ead9b2f57abf7beecb4954604554f81fd59
ser888000 <cto8@ya.ru>
2022-05-28 16:16:50 +0300

    222

--- test_items.py
+++ test_items.py
@@ -5,7 +5,7 @@ import time
 
 class TestButton:
     def test_button(self, browser):
-        link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
         browser.get(link)
         button = browser.find_element(By.CSS_SELECTOR, "button[type='submit']")
 #        time.sleep(30)
